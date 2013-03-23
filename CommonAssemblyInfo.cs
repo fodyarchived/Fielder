7b2f88ac76bdfc43ff662ae690ae263b61236b3d
Simon Cropp <simon.cropp@gmail.com>
2013-03-23 21:40:15 +1100

    move to fody nuget

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Fielder")]
 [assembly: AssemblyProduct("Fielder")]
-[assembly: AssemblyVersion("0.13.0.0")]
-[assembly: AssemblyFileVersion("0.13.0.0")]
+[assembly: AssemblyVersion("0.14.0.0")]
+[assembly: AssemblyFileVersion("0.14.0.0")]
