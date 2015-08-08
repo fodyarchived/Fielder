982cd96f272fa9839d9f80c5807ae414a244293a
Simon Cropp <simon.cropp@gmail.com>
2015-08-08 17:09:44 +1000

    add missing file

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Fielder")]
 [assembly: AssemblyProduct("Fielder")]
-[assembly: AssemblyVersion("1.0.9")]
-[assembly: AssemblyFileVersion("1.0.9")]
+[assembly: AssemblyVersion("1.0.10")]
+[assembly: AssemblyFileVersion("1.0.10")]
