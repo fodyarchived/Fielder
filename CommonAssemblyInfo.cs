6db408b9fa93ac22fc50c0ee8f06aff2cc96e0b0
Simon Cropp <simon.cropp@gmail.com>
2017-04-23 14:02:33 +1000

    fix nuspec

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Fielder")]
 [assembly: AssemblyProduct("Fielder")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
