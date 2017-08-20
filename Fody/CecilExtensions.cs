c5e6b5292a4781e634658c147f20c093821797f1
Simon Cropp <simon.cropp@gmail.com>
2017-08-20 19:15:38 +1000

    cleanup

--- Fody/CecilExtensions.cs
+++ Fody/CecilExtensions.cs
@@ -7,15 +7,16 @@ public static class CecilExtensions
 
     public static bool IsRefOrOut(this Instruction next)
     {
-        if (next.OpCode == OpCodes.Call || next.OpCode == OpCodes.Calli)
+        if (next.OpCode != OpCodes.Call && next.OpCode != OpCodes.Calli)
         {
-            var methodReference = next.Operand as MethodReference;
-            if (methodReference != null)
-            {
-                return methodReference.Parameters.Any(x => x.IsOut || x.ParameterType.Name.EndsWith("&"));
-            }
+            return false;
         }
-        return false;
+        var methodReference = next.Operand as MethodReference;
+        if (methodReference == null)
+        {
+            return false;
+        }
+        return methodReference.Parameters.Any(x => x.IsOut || x.ParameterType.Name.EndsWith("&"));
     }
 
 }
